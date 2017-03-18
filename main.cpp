5a0e431ebb5ebaacf7ff848fd6bbdbb954efb582
blasdch18 <blas.cruz@ucsp.edu.pe>
2017-03-18 16:11:11 -0500

    conflictos resueltos

--- main.cpp
+++ main.cpp
@@ -6,12 +6,16 @@ int main(){
 
 	cout<<"Hello World! :D"<<endl;
 
+
 	for(int i=0; i<10; ++i){
         cout<<"Angel"<<endl;
 	}
 
 	cout<<"Fabricio Ballon editó esto"<<endl;
 
+	cout<<"2+2=5  :O"<<endl;
+	
 	return 0;
+	
 
 }
