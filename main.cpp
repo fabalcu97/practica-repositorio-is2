55b9f2a4df20e76c8feec70d36d6f5708aa53314
ALUMNO-G <ALUMNO-G@UCSP.AULAB>
2017-03-18 16:11:28 -0500

    includes

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
-
+#include<stdlib.h>
+#include<stdio.h>
 using namespace std;
 
 int main(){
-	
+
 	cout<<"Hello World! :D"<<endl;
-	
+	printf("%s,Hello world");
 	return 0;
-	
+
 }
